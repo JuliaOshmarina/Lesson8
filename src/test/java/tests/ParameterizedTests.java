217be2fdfecd87b21a272448fd3a99a9aee06ca8
JuliaOshmarina <ju99478@gmail.com>
2025-01-12 21:59:12 +0500

    Added pravki

--- src/test/java/tests/ParameterizedTests.java
+++ src/test/java/tests/ParameterizedTests.java
@@ -2,10 +2,8 @@ package tests;
 
 import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -15,24 +13,22 @@ import java.util.stream.Stream;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
+@Tag("SMOKE")
 public class ParameterizedTests extends TestBase{
 
-    @Test
     @ValueSource(strings =  {"О магазине", "Новости", "Контакты"})
-    @Tag("SMOKE")
     @ParameterizedTest(name = "Проверка наличия главных кнопок меню")
-    public void parameterMenuTest(String Menu) {
-        $("[id=top-menu]").$((byText(Menu))).click();
-        $("div#workarea h2").shouldHave(Condition.text(Menu));
+    public void parameterMenuTest(String menu) {
+        $("[id=top-menu]").$((byText(menu))).click();
+        $("div#workarea h2").shouldHave(Condition.text(menu));
     }
 
 
     @CsvFileSource(resources = "/test_data/searchCategoryMenu.csv" )
-    @Tag("SMOKE")
     @ParameterizedTest(name = "Проверка наличия категорий на странице")
-    public void parameterCategoryTest(String Category) {
-        $("[id=left-menu]").$((byText(Category))).click();
-        $("div#workarea h2").shouldHave(Condition.text(Category));
+    public void parameterCategoryTest(String category) {
+        $("[id=left-menu]").$((byText(category))).click();
+        $("div#workarea h2").shouldHave(Condition.text(category));
     }
 
 
@@ -41,11 +37,10 @@ public class ParameterizedTests extends TestBase{
     }
 
     @MethodSource("menuCategoryTest")
-    @Tag("SMOKE")
     @ParameterizedTest(name = "Проверка наличия главных кнопок меню stream")
-    public void menuCategoryTest(String MenuStream) {
-        $("[id=top-menu]").$((byText(MenuStream))).click();
-        $("div#workarea h2").shouldHave(Condition.text(MenuStream));
+    public void menuCategoryTest(String menuStream) {
+        $("[id=top-menu]").$((byText(menuStream))).click();
+        $("div#workarea h2").shouldHave(Condition.text(menuStream));
     }
 
 
